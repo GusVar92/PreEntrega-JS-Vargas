415f04aab302f949bbdd2c1a76391ddf0c6f743f
GusVar92 <v.gustavo1992@gmail.com>
2024-04-02 22:36:59 -0300

    Comentarios agregados

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ alert ("Bienvenido al simulador de carrito de compras.")
 let n = prompt("Seleccione la opcion deseada:\n1-Armar carrito\n2-Calcular Cuotas\n3-Calcular descuento\n0-Salir")
 
 if(n==1){
+    //Armado basico de lista (por ahora 1 articulo, añadir funcion para guardar items y mostrar lista para la siguiente pre-entrega)
     const prod = prompt("Por favor indique el articulo:");
     const precio = parseFloat (prompt("Por favor indique su precio"));
     const articulo = prod + " $" + precio +"\n";
@@ -15,14 +16,17 @@ if(n==1){
     montoCuota = calcularMontoCuota(total, coutas, interes);
     alert ("El valor de cada cuota es: $" + montoCuota);
 }else if(n==3){
+    //Calculadora de importe
     let importe = parseFloat(prompt("Ingrese el importe total de su compra:"));
     let desc = parseInt(prompt("Ingrese el descuento a calcular:"));
     totalDesc = importe - ((desc / 100) * importe);
     alert("El total a pagar es: " + totalDesc);
 }else if(n==0){
+    //Mensaje de finalizacion
     alert("Gracias, que tenga un buen dia.");
 }
 else{
+    //Mensaje de error
     alert("Opcion incorrecta.");
 }
 
