fda9c016df50e3099a1e2eae43f5678fd9c73550
GusVar92 <v.gustavo1992@gmail.com>
2024-05-05 17:21:08 -0300

    funciones agregadas

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
-alert ("Bienvenido al simulador de carrito de compras.")
-let n = prompt("Seleccione la opcion deseada:\n1-Armar carrito\n2-Calcular Cuotas\n3-Calcular descuento\n0-Salir")
-
-if(n==1){
+function armarCarrito(){
     //Armado basico de lista (por ahora 1 articulo, añadir funcion para guardar items y mostrar lista para la siguiente pre-entrega)
-    const prod = prompt("Por favor indique el articulo:");
-    const precio = parseFloat (prompt("Por favor indique su precio"));
-    const articulo = prod + " $" + precio +"\n";
+    let prod = prompt("Por favor indique el articulo:");
+    let precio = parseFloat (prompt("Por favor indique su precio"));
+    let articulo = prod + " $" + precio +"\n";
     alert ("Producto añadido con exito.");
-    alert (articulo); 
-}else if(n==2){
+    alert (articulo);
+}
+
+function calculadoraDeCoutas(){
     //Funcion de lista, muestra los producctos en la opcion 1.
     let total = parseFloat(prompt("Ingrese el total de su compra o producto:"));
     let coutas = parseInt(prompt("Ingrese la cantidad de cuotas:"));
     let interes = parseFloat(prompt("Ingrese el interes acordado:"));
     montoCuota = calcularMontoCuota(total, coutas, interes);
     alert ("El valor de cada cuota es: $" + montoCuota);
-}else if(n==3){
-    //Calculadora de importe
-    let importe = parseFloat(prompt("Ingrese el importe total de su compra:"));
-    let desc = parseInt(prompt("Ingrese el descuento a calcular:"));
-    totalDesc = importe - ((desc / 100) * importe);
-    alert("El total a pagar es: " + totalDesc);
-}else if(n==0){
-    //Mensaje de finalizacion
-    alert("Gracias, que tenga un buen dia.");
-}
-else{
-    //Mensaje de error
-    alert("Opcion incorrecta.");
 }
 
 function calcularMontoCuota(total, cuotas, interes) {
@@ -36,4 +22,38 @@ function calcularMontoCuota(total, cuotas, interes) {
     let montoInteres = (total * (interes / 100)) / cuotas;
     let montoCuota = montoSinInteres + montoInteres;
     return montoCuota;
+}
+
+function calculadorDeDescuento(){
+    //Calculadora de importe
+    let importe = parseFloat(prompt("Ingrese el importe total de su compra:"));
+    let desc = parseInt(prompt("Ingrese el descuento a calcular:"));
+    totalDesc = importe - ((desc / 100) * importe);
+    alert("El total a pagar es: " + totalDesc);
+}
+
+function welcome(){
+    alert ("Bienvenido al simulador de carrito de compras.")
+    let opcion = prompt("Seleccione la opcion deseada:\n1-Armar carrito\n2-Calcular Cuotas\n3-Calcular descuento\n0-Salir")
+    return opcion;
+}
+
+welcome();
+
+let n = welcome();
+
+while(n != "1" && n != "2" && n != "3" && n != "0"){
+    alert("Opcion incorrecta");
+    n = welcome();
+}
+
+if(n==1){
+    armarCarrito();
+}else if(n==2){
+    calculadoraDeCoutas();
+}else if(n==3){
+    calculadorDeDescuento();
+}else{
+    //Mensaje de finalizacion
+    alert("Gracias, que tenga un buen dia.");
 }
